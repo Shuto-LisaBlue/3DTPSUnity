31572eb183f240ef095671f0b3cb6e88468c05d3
LisaBlue <dblueanddred@live.jp>
2018-02-06 17:01:41 +0900

    first commit

---
+++ Assets/Script/PlayerMoveScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMoveScript : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Animator _animator;
+    private Transform _transform;
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _transform = GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        var v = Input.GetAxis("Vertical");
+        _animator.SetFloat("Speed", v);
+
+        var h = Input.GetAxis("Horizontal");
+        _animator.SetFloat("Direction", h);
+
+        var jump = Input.GetAxis("Jump");
+        _animator.SetBool("Jump", jump != 0);
+
+        if (v > 0) _transform.Translate(0.0f, 0.0f, v / 10.0f);
+        else _transform.Translate(0.0f, 0.0f, v / 30.0f);
+        _transform.Rotate(new Vector3(0.0f, h * 2.0f, 0.0f));
+
+    }
+}
+
