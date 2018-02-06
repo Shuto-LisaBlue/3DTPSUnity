4fc99972db9fb1d4843dfb2e5e86c2ca26e76053
LisaBlue <dblueanddred@live.jp>
2018-02-07 01:49:34 +0900

    弾が出て、消えるように

---
+++ Assets/BulletActionScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BulletActionScript : MonoBehaviour {
+    private float Times = 0;
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Times += Time.deltaTime;
+        if (Times >= 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Script/ShotBullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBullet : MonoBehaviour {
+
+    public GameObject Bullet;
+    public Transform Shooter;
+    public float Speed = 10000.0f;
+    public float Shotrate = 0.5f;
+    private float LastShot = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LastShot += Time.deltaTime;
+		if(Input.GetMouseButton(0) && LastShot > Shotrate)
+        {
+            GameObject bullets = GameObject.Instantiate(Bullet) as GameObject;
+            Vector3 force;
+            force = this.gameObject.transform.forward * Speed;
+            bullets.GetComponent<Rigidbody>().AddForce(force,ForceMode.Impulse);
+            bullets.transform.position = Shooter.position;
+
+            LastShot = 0;
+        }
+	}
+}
