37bfab0f021c97938b3812be4673940e92c1508a
LisaBlue <dblueanddred@live.jp>
2018-02-14 20:39:44 +0900

    体力0の時、フェードアウトするように

---
+++ Assets/Script/EndFadeOut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndFadeOut : MonoBehaviour {
+
+    public GameObject PC1;
+    public GameObject PC2;
+    PlayerLife _playerLifePC1;
+    PlayerLife _playerLifePC2;
+    private float alpha = 0f;
+
+    void Start () {
+        _playerLifePC1 = PC1.GetComponent<PlayerLife>();
+        _playerLifePC2 = PC2.GetComponent<PlayerLife>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (_playerLifePC1.KnockOut || _playerLifePC2.KnockOut)
+        {
+            GetComponent<Image>().color = new Color(0f, 0f, 0f, alpha);
+            alpha += 0.01f;
+        }
+    }
+}
